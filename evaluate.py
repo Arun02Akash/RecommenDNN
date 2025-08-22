d08f8d04f084c52708df231588b29ec93192254b
Arun02Akash <83970426+Arun02Akash@users.noreply.github.com>
2025-08-22 15:58:16 -0500

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,86 +1,82 @@
-'''
-Created on Apr 15, 2016
-Evaluate the performance of Top-K recommendation:
-    Protocol: leave-1-out evaluation
-    Measures: Hit Ratio and NDCG
-    (more details are in: Xiangnan He, et al. Fast Matrix Factorization for Online Recommendation with Implicit Feedback. SIGIR'16)
-
-@author: hexiangnan
-'''
 import math
-import heapq # for retrieval topK
-import multiprocessing
+import heapq
+import multiprocessing as mp
 import numpy as np
-from time import time
-#from numba import jit, autojit
 
-# Global variables that are shared across processes
-_model = None
-_testRatings = None
-_testNegatives = None
-_K = None
 
-def evaluate_model(model, testRatings, testNegatives, K, num_thread):
+def evaluate_model(model, test_ratings, test_negatives, K: int, num_threads: int = 1):
     """
-    Evaluate the performance (Hit_Ratio, NDCG) of top-K recommendation
-    Return: score of each test rating.
+    Evaluate recommendation performance with leave-one-out.
+
+    Args:
+        model: Keras model for prediction.
+        test_ratings: List of [user, ground_truth_item].
+        test_negatives: List of negative items for each user.
+        K: Number of top items for evaluation.
+        num_threads: Number of parallel threads.
+
+    Returns:
+        hits: List[int], hit ratio per test case.
+        ndcgs: List[float], NDCG score per test case.
     """
-    global _model
-    global _testRatings
-    global _testNegatives
-    global _K
-    _model = model
-    _testRatings = testRatings
-    _testNegatives = testNegatives
-    _K = K
-        
-    hits, ndcgs = [],[]
-    if(num_thread > 1): # Multi-thread
-        pool = multiprocessing.Pool(processes=num_thread)
-        res = pool.map(eval_one_rating, range(len(_testRatings)))
-        pool.close()
-        pool.join()
-        hits = [r[0] for r in res]
-        ndcgs = [r[1] for r in res]
-        return (hits, ndcgs)
-    # Single thread
-    for idx in xrange(len(_testRatings)):
-        (hr,ndcg) = eval_one_rating(idx)
-        hits.append(hr)
-        ndcgs.append(ndcg)      
-    return (hits, ndcgs)
+    if num_threads > 1:
+        with mp.Pool(processes=num_threads) as pool:
+            res = pool.map(
+                lambda idx: _eval_one_rating(model, test_ratings, test_negatives, K, idx),
+                range(len(test_ratings))
+            )
+        hits, ndcgs = zip(*res)
+    else:
+        hits, ndcgs = [], []
+        for idx in range(len(test_ratings)):
+            hr, ndcg = _eval_one_rating(model, test_ratings, test_negatives, K, idx)
+            hits.append(hr)
+            ndcgs.append(ndcg)
 
-def eval_one_rating(idx):
-    rating = _testRatings[idx]
-    items = _testNegatives[idx]
-    u = rating[0]
-    gtItem = rating[1]
-    items.append(gtItem)
-    # Get prediction scores
-    map_item_score = {}
-    users = np.full(len(items), u, dtype = 'int32')
-    predictions = _model.predict([users, np.array(items)], 
-                                 batch_size=100, verbose=0)
-    for i in xrange(len(items)):
-        item = items[i]
-        map_item_score[item] = predictions[i]
-    items.pop()
-    
-    # Evaluate top rank list
-    ranklist = heapq.nlargest(_K, map_item_score, key=map_item_score.get)
-    hr = getHitRatio(ranklist, gtItem)
-    ndcg = getNDCG(ranklist, gtItem)
-    return (hr, ndcg)
+    return list(hits), list(ndcgs)
 
-def getHitRatio(ranklist, gtItem):
-    for item in ranklist:
-        if item == gtItem:
-            return 1
-    return 0
 
-def getNDCG(ranklist, gtItem):
-    for i in xrange(len(ranklist)):
-        item = ranklist[i]
-        if item == gtItem:
-            return math.log(2) / math.log(i+2)
-    return 0
+def _eval_one_rating(model, test_ratings, test_negatives, K: int, idx: int):
+    """
+    Evaluate a single test rating.
+
+    Args:
+        model: Keras model.
+        test_ratings: Ground-truth pairs.
+        test_negatives: Negative items per test case.
+        K: Top-K.
+        idx: Index of test rating.
+
+    Returns:
+        (hit, ndcg)
+    """
+    user, gt_item = test_ratings[idx]
+    items = list(test_negatives[idx])  # copy negatives
+    items.append(gt_item)
+
+    # Predict scores
+    users = np.full(len(items), user, dtype=np.int32)
+    predictions = model.predict([users, np.array(items)], batch_size=100, verbose=0).flatten()
+
+    # Map items to predicted scores
+    item_score = {item: predictions[i] for i, item in enumerate(items)}
+
+    # Get top-K items
+    ranklist = heapq.nlargest(K, item_score, key=item_score.get)
+
+    hr = get_hit_ratio(ranklist, gt_item)
+    ndcg = get_ndcg(ranklist, gt_item)
+    return hr, ndcg
+
+
+def get_hit_ratio(ranklist, gt_item) -> int:
+    """Return 1 if ground-truth item is in top-K, else 0."""
+    return int(gt_item in ranklist)
+
+
+def get_ndcg(ranklist, gt_item) -> float:
+    """Compute Normalized Discounted Cumulative Gain (NDCG)."""
+    if gt_item in ranklist:
+        index = ranklist.index(gt_item)
+        return math.log(2) / math.log(index + 2)
+    return 0.0
